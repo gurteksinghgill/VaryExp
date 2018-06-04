7b0c9f5d7216b4f771d5ac07963a8acff170735d
Peter Straka <straka.ps@gmail.com>
2018-06-04 12:07:31 +0800

    simplify varyingexp1 example

--- R/varyExp-varyingexp1.R
+++ R/varyExp-varyingexp1.R
@@ -1,3 +1,7 @@
+library(ggplot2)
+library(tidyverse)
+
+source("R/varyExp-DTSM.R")
 #Parameters
 alpha <- function(x)
   0.5 / (1 + exp(-x)) + 0.25
@@ -6,39 +10,16 @@ Psi <- function(x,t) {
   r[r>1] <- 1
   r
 }
-a <- function(x, s)
-  (1) ^ (-alpha(x))
-b <- function(x,s)
-  0
-d <- function(x,s) 
-  0
 
 #Set up grid size and spacing parameters
-c <- 100;
-chi <- 1 / sqrt(c)
+c <- 10
 xrange <- c(-2,2)
-tau <- (10/c)
-age_max <- round(100/tau + 1)*tau
+chi <- 0.1 / sqrt(c)
+tau <- 1/c
+snapshots <- c(1,5,100)
+
+out1 <- DTSM(xrange = xrange, snapshots = snapshots, c = c, chi = chi, tau = tau, Psi = Psi)
+
+plot_DTSM_output(out1)
 
 
-#Evaluate densities at multiple times 
-p1 <- DTSM(xrange, T = 100, Psi, a, b, d);
-# p2 <- DTSM(xrange, T = 2000, Psi, a, b, d);
-# p5 <- DTSM(xrange, T = 5000, Psi, a, b, d);
-# p10 <- DTSM(xrange, T = 10000, Psi, a, b, d);
-# p15 <- DTSM(xrange, T = 15000, Psi, a, b, d);
-# p25 <- DTSM(xrange, T = 25000, Psi, a, b, d);
-
-#plot densities
-par(mfrow=c(1,1));
-par(fig = c(0,1,0,1));
-plot(p1$x, p1$Px, type="l", xlab = "x", ylab = paste("P(x,t)"));
-# lines(p2$x, p2$Px, type="l", col="blue");
-# lines(p5$x, p5$Px, type="l", col="pink");
-# lines(p10$x, p10$Px, type="l", col="yellow");
-# lines(p15$x, p15$Px, type="l", col="green");
-# lines(p25$x, p25$Px, type="l", col="red");
-grid();
-legend("topleft",c(expression(10^3),expression(2 %*% 10^3),expression(5 %*% 10^3),expression(10 %*% 10^3),expression(15 %*% 10^3),expression(25 %*% 10^3) ), title = "Densities at t =", lty = c(1,1,1,1,1,1), col=c("black","blue","pink","yellow","green","red") )
-par(fig = c(0.55,0.99, 0.55, 0.99), new = T) 
-plot(seq(-5,5,0.01), alpha((seq(-5,5,0.01))), xlab="x", ylab=expression(alpha (x)), type="l")
