f6b3df78e2f90218ec7b09af0c1d9d3bbf9e5aff
Peter Straka <straka.ps@gmail.com>
2018-06-04 14:09:42 +0800

    varyingexp1 works

--- R/varyExp-varyingexp1.R
+++ R/varyExp-varyingexp1.R
@@ -1,25 +1,13 @@
-library(ggplot2)
-library(tidyverse)
-
 source("R/varyExp-DTSM.R")
 #Parameters
 alpha <- function(x)
   0.5 / (1 + exp(-x)) + 0.25
 Psi <- function(x,t) {
-  r <- (t^(-alpha(x))  )/gamma(1-alpha(x)) 
-  r[r>1] <- 1
-  r
+  if (t <= 0)
+    Inf
+  else 
+    (t^(-alpha(x))  )/gamma(1-alpha(x)) 
 }
 
-#Set up grid size and spacing parameters
-c <- 10
-xrange <- c(-2,2)
-chi <- 0.1 / sqrt(c)
-tau <- 1/c
-snapshots <- c(1,5,100)
-
-out1 <- DTSM(xrange = xrange, snapshots = snapshots, c = c, chi = chi, tau = tau, Psi = Psi)
-
+out1 <- DTSM(Psi = Psi, xrange = c(-4,4), snapshots = c(1,2,4))
 plot_DTSM_output(out1)
-
-
